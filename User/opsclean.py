2db2ce4aa4ef5c1599f4311a75ded1c24cfc1f32
Amelia Aronsohn <squirrel@wearing.black>
2023-01-02 16:33:06 -0800

    Replace old plugins likely not needed anymore

--- User/opsclean.py
+++ User/opsclean.py
@@ -1,13 +1,12 @@
-import sublime
 import sublime_plugin
 
 
 class OpsCleanCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		links = self.view.find_all(' \(.+\)$')
+		links = self.view.find_all(r' \(.+\)$')
 		for link in reversed(links):
 			self.view.erase(edit, link)
 
-		headers = self.view.find_all('^\*')
+		headers = self.view.find_all(r'^\*')
 		for header in reversed(headers):
 			self.view.replace(edit, header, '###')
