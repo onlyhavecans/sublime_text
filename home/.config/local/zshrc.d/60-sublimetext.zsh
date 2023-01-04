8d5daac999b41f30f459432082d5ccb3b5b713da
Amelia Aronsohn <squirrel@wearing.black>
2023-01-04 11:09:18 -0800

    Add default sublime shell to this repo

---
+++ home/.config/local/zshrc.d/60-sublimetext.zsh
@@ -0,0 +1,7 @@
+#shellcheck shell=sh
+
+# Sublime over Vim where available
+if [ -d "/Applications/Sublime Text.app" ]; then
+  alias vim="subl --new-window"
+  export EDITOR="subl --new-window --wait"
+fi
